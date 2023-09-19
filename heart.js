ccd2970fe7ca8992e9282b897534cf92c5c6ce86
ajibola <yekinnijibola@gmail.com>
2023-09-19 12:06:53 +0100

    fix: sorted alignment issues

--- heart.js
+++ heart.js
@@ -1,18 +1,17 @@
-
-$(document).ready(function(){
-    $(".button").click(function(){
-        $("#first").fadeOut(500);
-		$("#second").fadeOut(500);
-		$("#third").fadeOut(500);
-		$("footer").fadeOut(500);
-		$("#popout").fadeIn("slow");
-    });
+$(document).ready(function () {
+  $(".button").click(function () {
+    $("#first").fadeOut(500);
+    $("#second").fadeOut(500);
+    $("#third").fadeOut(500);
+    $("footer").fadeOut(500);
+    $("#popout").fadeIn("slow");
+  });
 });
 function myFunction(x) {
-    x.classList.toggle("change");
-	 $("#first").fadeIn(500);
-		$("#second").fadeIn(500);
-		$("#third").fadeIn(500);
-		$("footer").fadeIn(500);
-		$("#popout").fadeOut("slow");
+  x.classList.add("change");
+  $("#first").fadeIn(500);
+  $("#second").fadeIn(500);
+  $("#third").fadeIn(500);
+  $("footer").fadeIn(500);
+  $("#popout").fadeOut("slow");
 }
